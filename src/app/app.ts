cfc6855f3cdc8655afa9128f561f23d08f697599
Mohamed Traore <traoremohamed7212@gmail.com>
2025-07-22 20:25:13 +0000

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayingCard } from "./components/playing-card/playing-card";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PlayingCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/playing-card/playing-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playing-card',
+  imports: [],
+  templateUrl: './playing-card.html',
+  styleUrl: './playing-card.css'
+})
+export class PlayingCard {
+
+}
