cd1de3229eb5788bfa6b5fbd7af1a669f2944f5e
Mohamed <traoremohamed7212@gmail.com>
2025-07-29 17:37:28 +0000

    terminer avec  les recherche

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,13 +11,15 @@ import { SearchBar } from './components/search-bar/search-bar';
   styleUrl: './app.css'
 })
 export class App {
-  
+  search=''; 
   protected readonly title = signal('playing-cards');
    monster1! : Monster;
+   monster2 : Monster = new Monster();
   constructor(){
+    this.monster2.name="Mohamed";
     this.monster1 = new Monster();
     this.monster1.name="Pik";
-    this.monster1.hp=400;
+    this.monster1.hp=10000;
     this.monster1.figureCaption ="N°002";
   }
   count : number = 0;
--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,14 +1,21 @@
-import { Component, EventEmitter, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.css'
 })
 export class SearchBar {
+  @Input() search = 'initial';
   @Output() buttonCliked = new EventEmitter();
+  @Output() searchChange = new EventEmitter<string>();
   searchButtonCliked(){
      this.buttonCliked.emit();
+  }
+  updateSearch(value:string){
+    this.searchChange.emit(value);
   }
 }
